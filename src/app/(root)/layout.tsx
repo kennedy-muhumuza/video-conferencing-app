b40cd727785e3bcf130ba67d8ed718e24c394b52
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-09-12 20:11:24 +0300

    first commit

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import React, { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      {/* <Navbar /> */}
+      {children}
+      {/* <Sidebar /> */}
+    </main>
+  );
+};
+
+export default RootLayout;
