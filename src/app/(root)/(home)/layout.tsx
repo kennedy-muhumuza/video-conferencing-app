9c9d066b98cf51913746e3a2276574c2d25fa3a3
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-10-04 01:02:23 +0300

    update home page

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import React, { ReactNode } from "react";
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="relative">
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="flex">
         <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-28 pt-28 max-md:pb-14 sm:px-14 ">
